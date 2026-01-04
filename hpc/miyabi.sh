4df43c82d5dc34343632e055b7396716e97ddc34
34j <34j.github@proton.me>
2026-01-04 17:56:39 +0900

    Update miyabi.sh

--- hpc/miyabi.sh
+++ hpc/miyabi.sh
@@ -3,11 +3,13 @@
 #PBS -o out.log
 #PBS -j oe
 #PBS -m abe
-#PBS -V
+#PBS -V select=2:mpiprocs=1
 #PBS -l select=1
 #PBS -l walltime=03:00
 #PBS -l mail_power_info=true
 
 #export UV_CACHE_DIR=/work//m
+echo $UV_CACHE_DIR
 cd ${PBS_O_WORKDIR}
 uv run python -m hpc_tutorial.tutorial_normal
+uv run python -m hpc_tutorial.tutorial_dask
