cd9f51e581a24c04fdf92510e9b6b3dbd0122f7f
34j <34j.github@proton.me>
2026-01-04 14:48:41 +0900

    chore: initial commit

---
+++ src/hpc_tutorial/__main__.py
@@ -0,0 +1,5 @@
+"""Make the CLI runnable using python -m hpc_tutorial."""
+
+from .cli import app
+
+app(prog_name="hpc-tutorial")
---
+++ tests/test_main.py
@@ -0,0 +1,6 @@
+from hpc_tutorial.main import add
+
+
+def test_add():
+    """Adding two number works as expected."""
+    assert add(1, 1) == 2
