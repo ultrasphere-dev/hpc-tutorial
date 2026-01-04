b4e935299d9dcd2269c4aec4ed5b3729dec46a6e
34j <34j.github@proton.me>
2026-01-04 17:54:10 +0900

    chore: fix script

--- hpc/miyabi.sh
+++ hpc/miyabi.sh
@@ -3,8 +3,11 @@
 #PBS -o out.log
 #PBS -j oe
 #PBS -m abe
+#PBS -V
 #PBS -l select=1
 #PBS -l walltime=03:00
+#PBS -l mail_power_info=true
 
+#export UV_CACHE_DIR=/work//m
 cd ${PBS_O_WORKDIR}
 uv run python -m hpc_tutorial.tutorial_normal
